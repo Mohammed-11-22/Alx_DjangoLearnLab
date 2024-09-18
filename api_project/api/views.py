851cfeb80429bcfbea40386bf7c141fd7ec3f0d1
Mohammed-11-22 <mrmedmohammed21@gmail.com>
2024-09-18 20:18:08 +0100

    removing the folder

--- api_project/api/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework.generics import ListAPIView # type: ignore
-from .models import Book
-from .serializers import BookSerializer
-
-class BookList(ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
